d0576c365fbaeefb075b4d59f5d0781439415232
patrickdurke <50921587+patrickdurke@users.noreply.github.com>
2022-09-14 17:46:03 +0200

    CORS enabled

--- Learning-Platform-Server/Program.cs
+++ Learning-Platform-Server/Program.cs
@@ -1,11 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowRequestsFromBPR-Learning-Platform-Frontend",
+        policy =>
+        {
+            policy.WithOrigins("https://bpr-learning-platform.github.io/",
+                                "http://localhost:4200")
+                    .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
