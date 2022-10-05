17bc4492c0fe84b4b7256556331a96b366c3fefb
patrickdurke <50921587+patrickdurke@users.noreply.github.com>
2022-10-05 15:02:10 +0200

    GET Tasks endpoint now returns batch of 3 random tasks + added Type to Task

--- Learning-Platform-Server/Entities/MongoDbTask.cs
+++ Learning-Platform-Server/Entities/MongoDbTask.cs
@@ -3,25 +3,33 @@ using System.Runtime.Serialization;
 
 namespace Learning_Platform_Server.Entities
 {
-    public class TaskRoot
+    public class MongoDbTaskRoot
     {
         [JsonProperty("_id")]
         public Id? Id { get; set; }
         public MongoDbTask? Task { get; set; }
+        public TaskId? TaskId { get; set; }
+        public override string ToString() => "MongoDbTaskRoot: Oid: " + Id + ", " + Task + ", TaskId: " + TaskId;
+    }
+    public class TaskId
+    {
+        [JsonProperty("$numberLong")]
+        public string? NumberLong { get; set; }
+        public override string ToString() => NumberLong + "";
     }
 
     public class MongoDbTask
     {
-        public string? TaskID { get; set; }
         public string? Step { get; set; }
+        public string? Type { get; set; }
         public string? Difficulty { get; set; }
         public string? Exercise { get; set; }
         public string? Answer { get; set; }
 
         public override string ToString()
         {
-            return "MongoDbTask: taskid: " + TaskID +
-                ", step: " + Step +
+            return "MongoDbTask: step: " + Step +
+                ", type: " + Type +
                 ", difficulty: " + Difficulty +
                 ", exercise: " + Exercise +
                 ", answer: " + Answer;
